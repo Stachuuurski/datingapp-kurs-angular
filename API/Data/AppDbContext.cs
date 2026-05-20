ee812f4aaaa1b741eb48d5ebea6dec875d237955
Stachuuurski <stas.kalemba2009@gmail.com>
2026-05-20 14:33:22 +0200

    rozszerzanie API

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -1,11 +1,15 @@
 using System;
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace API.Data;
 
     public class AppDbContext(DbContextOptions options) : DbContext(options)
     {
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        
     }
 
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,5 @@
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace API.Entities;
 
 public class AppUser
@@ -5,8 +7,11 @@ public class AppUser
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public required string DisplayName { get; set; }
     public required string Email { get; set; }
+    public string? ImageUrl{get; set;}
 
     public required byte[] PasswordHash { get; set; }
 
     public required byte[] PasswordSalt { get; set; }
+
+    public Member Member {get; set;} = null!;
 }
